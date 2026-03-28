1534731a1b6511c94cccd5c3f35e31781994b2cc
Harsh <har1999@users.noreply.github.com>
2026-03-28 12:34:48 +0530

    clear-repo

--- labourlink/static/js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const revealables = document.querySelectorAll('.reveal');
-    revealables.forEach((node, index) => {
-        setTimeout(() => {
-            node.classList.add('is-visible');
-        }, 120 * index);
-    });
-});
